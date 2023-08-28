ea094a1ca3df9949b7750b33f2f92ad45cf19857
Madhatter <kraehenkind@googlemail.com>
2023-08-28 09:29:54 +0200

    small update

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print("Hello World")
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.graphics import Rectangle
+
+
+class Racket(Widget):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def moveRacket(self):
+        pass
+
+class PongApp(App):
+    
+    def build(self):
+        root_widget = Racket()
+        return root_widget
+
+if __name__ == '__main__':
+    PongApp().run()
