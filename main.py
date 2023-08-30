0d2bd25a77f9880291949ddb56fde8437b150cba
kraehenkind <kraehenkind@googlemail.com>
2023-08-30 11:39:18 +0200

    next commit

--- main.py
+++ main.py
@@ -6,16 +6,21 @@ from kivy.graphics import Rectangle
 class Racket(Widget):
 
     def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-
+        super(Racket,self).__init__(**kwargs)
+        with self.canvas:
+            self.rect = Rectangle(pos=(325,10), size=(150,25))
+        
     def moveRacket(self):
         pass
+
+
 
 class PongApp(App):
     
     def build(self):
         root_widget = Racket()
         return root_widget
+        
 
 if __name__ == '__main__':
     PongApp().run()
